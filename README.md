ff6c7d6b1d138c97b26f78fa28fa1503add5f540
IsaqueZaratustra <ADSisaque@gmail.com>
2022-07-28 17:40:18 -0300

    add

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 <h1>Cálculo IMC</h1>
-
+<img src = "/img/exemplo-peso-normal.png" alt="peso normal">
 O IMC é reconhecido como padrão internacional para avaliar o grau de sobrepeso e obesidade. É calculado dividindo o peso (em kg) pela altura ao quadrado (em metros).
 
 IMC é a sigla para Índice de Massa Corpórea, parâmetro adotado pela Organização Mundial de Saúde para calcular o peso ideal de cada pessoa.
--- src/imc-main.cpp
+++ src/imc-main.cpp
@@ -5,13 +5,16 @@ int main()
 {
     float altura, peso, imc;
 
-    std::cout << "Digite o seu peso (Kg): " ;
+    std::cout << "IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII" << std::endl;
+    std::cout << "I Digite o seu peso (Kg):                    I" ;
     std::cin >> peso;
-    std::cout << "Digite a sua altura (metro e centimetro): " ;
+    std::cout << "IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII"<< std::endl;
+    std::cout << "I Digite a sua altura (metro e centimetro):  I" ;
     std::cin >> altura;
-
+    std::cout << "IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII"<< std::endl;
     imc = peso /(altura * altura);
-    std::cout << "Seu IMC: " << imc << std::endl;
+    std::cout << "I Seu IMC: " << imc <<"                           I"<< std::endl;
+    std::cout << "IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII" << std::endl;
 
     situacao(imc);
 
