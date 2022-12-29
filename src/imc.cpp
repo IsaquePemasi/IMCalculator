46a2d4e5760b1f5a860be13b7643565b60a11c76
IsaquePemasi <ADSisaque@gmail.com>
2022-12-29 16:39:41 -0300

    add

--- src/imc.cpp
+++ src/imc.cpp
@@ -21,26 +21,106 @@ void situacao (float imc ){
     } else if (imc > 17 && imc < 18.5)
     {
     std::cout << "Magreza leve: abaixo do peso" << std::endl;    
-    std::cout << "pode apresentar: problemas de saude ligados a desnutricao " << std::endl;     
+    std::cout << "pode apresentar: problemas de saude ligados a desnutricao " << std::endl;  
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+
+
     }else if (imc > 18.5 && imc < 24.9)
     {
     std::cout << "Eutrofico: peso normal" << std::endl;    
-    std::cout << "Saudavel  " << std::endl;    
+    std::cout << "Saudavel  " << std::endl;
+    std::cout << " +--^----------,--------,-----,--------^-," << std::endl;
+    std::cout << " | |||||||||   `--------'     |          O" << std::endl;
+    std::cout << " `+---------------------------^----------|" << std::endl;
+    std::cout << "   ``_,---------,---------,--------------'" << std::endl;
+    std::cout << "     / XXXXXX /'|       /'" << std::endl;
+    std::cout << "    / XXXXXX /  ``    /'" << std::endl;
+    std::cout << "   / XXXXXX /`-------'" << std::endl;
+    std::cout << "  / XXXXXX /" << std::endl;
+    std::cout << " / XXXXXX /   " << std::endl;
+    std::cout << "(________(                " << std::endl;
+    std::cout << " `------'              " << std::endl;    
     }else if (imc > 25 && imc < 29.9)
     {
     std::cout << "Sobrepeso: acima do peso" << std::endl;    
-    std::cout << "pode apresentar: fadiga, varizes e ma circulacao " << std::endl;     
+    std::cout << "pode apresentar: fadiga, varizes e ma circulacao " << std::endl; 
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;    
     }else if (imc > 30 && imc < 34.9)
     {
     std::cout << "Obesidade 1" << std::endl;    
-    std::cout << "pode apresentar: diabetes, infarto, angina e aterosclerose " << std::endl;    
+    std::cout << "pode apresentar: diabetes, infarto, angina e aterosclerose " << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;    
     }else if (imc > 35 && imc < 39.9)
     {
     std::cout << "Obesidade 2: severa" << std::endl;    
-    std::cout << "pode apresentar: falta de ar e apneia do sono " << std::endl;     
+    std::cout << "pode apresentar: falta de ar e apneia do sono " << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;     
     }else if (imc > 40)
     {
     std::cout << "Obesidade 3: morbida" << std::endl;    
-    std::cout << "pode apresentar: refluxo infarto, avc e dificuldade de locomocao " << std::endl;    
+    std::cout << "pode apresentar: refluxo infarto, avc e dificuldade de locomocao " << std::endl; 
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;   
     }  
 }
