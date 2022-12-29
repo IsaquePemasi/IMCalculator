2e401a9e2e66a72518a2d22b15f6a0a9aeb8fa6a
IsaquePemasi <ADSisaque@gmail.com>
2022-12-29 16:46:54 -0300

    add

--- src/imc.cpp
+++ src/imc.cpp
@@ -22,29 +22,28 @@ void situacao (float imc ){
     {
     std::cout << "Magreza leve: abaixo do peso" << std::endl;    
     std::cout << "pode apresentar: problemas de saude ligados a desnutricao " << std::endl;  
-    std::cout << "" << std::endl;
-    std::cout << "" << std::endl;
-    std::cout << "" << std::endl;
-    std::cout << "" << std::endl;
-    std::cout << "" << std::endl;
-    std::cout << "" << std::endl;
-    std::cout << "" << std::endl;
-    std::cout << "" << std::endl;
-    std::cout << "" << std::endl;
-    std::cout << "" << std::endl;
-    std::cout << "" << std::endl;
-    std::cout << "" << std::endl;
-    std::cout << "" << std::endl;
-    std::cout << "" << std::endl;
-    std::cout << "" << std::endl;
-    std::cout << "" << std::endl;
-    std::cout << "" << std::endl;
-    std::cout << "" << std::endl;
-    std::cout << "" << std::endl;
-    std::cout << "" << std::endl;
-    std::cout << "" << std::endl;
-    std::cout << "" << std::endl;
-    std::cout << "" << std::endl;
+    std::cout << "               ,,ggddY888Ybbgg,," << std::endl;
+    std::cout << "          ,agd8""'   .d8888888888bga," << std::endl;
+    std::cout << "       ,gdP""'     .d88888888888888888g," << std::endl;
+    std::cout << "     ,dP``        ,d888888888888888888888b," << std::endl;
+    std::cout << "   ,dP         ,8888888888888888888888888b," << std::endl;
+    std::cout << "  ,8`          ,8888888P```88888888888888888," << std::endl;
+    std::cout << " ,8'           I888888I    )88888888888888888," << std::endl;
+    std::cout << ",8'            `8888888booo8888888888888888888," << std::endl;
+    std::cout << "d'              `88888888888888888888888888888b" << std::endl;
+    std::cout << "8                ``8888888888888888888888888888" << std::endl;
+    std::cout << "8                  ``88888888888888888888888888" << std::endl;
+    std::cout << "8                      ``8888888888888888888888" << std::endl;
+    std::cout << "Y,                        `8888888888888888888P" << std::endl;
+    std::cout << "`8,                         `88888888888888888'" << std::endl;
+    std::cout << " `8,              .oo.       `888888888888888'" << std::endl;
+    std::cout << "  `8a             8888        88888888888888'" << std::endl;
+    std::cout << "   `Yba           `""'       ,888888888888P'" << std::endl;
+    std::cout << "     `Yba                   ,88888888888``" << std::endl;
+    std::cout << "       `Yba,             ,8888888888P``                " << std::endl;
+    std::cout << "          `Y8baa,      ,d88888888P``" << std::endl;
+    std::cout << "               ````YYba8888P888``" << std::endl;
+
 
 
     }else if (imc > 18.5 && imc < 24.9)
